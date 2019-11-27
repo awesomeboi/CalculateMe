0903371d0cd94c3ab26e494c8e7a7c926fba7265
Volkan Sahin <vzsahin@yahoo.com>
2019-11-27 09:31:47 +0300

    Add files via upload
    **BUG FİX**
     In division, if there is an exception division to 0, program writes ERR on screen. User can make calculation with Err.
    Fixed; program not works until user click AC button.

--- CalculateMe/ViewController.swift
+++ CalculateMe/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
     var minusFlag = 0
     var multiplyFlag = 0
     var divideFlag = 0
+    var errFlag = 0
     var flagArray = [Int]()
     var calculationArray = ["+", "-", "x", "/"]
     
@@ -117,142 +118,101 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
-        if flagOfResult == 1{
-            flagOfResult = 0
-            resultLabel.text! = ""
-            numberShowed = ""
-            memoryNumber.text = ""
-        }
-        number = sender.tag
-        if resultLabel.text == "0"{
-            numberShowed = ""
-            numberCalculate = ""
-        }
-        visnumber = String(number)
-        calcnumber = String(number)
-        if resetPressed == false{
-            numberShowed = numberShowed + visnumber
-            numberCalculate = numberCalculate + calcnumber
-            resultLabel.text = numberShowed
-        }
-    }
-    
-    @IBAction func decimalPressed(_ sender: Any) {
-        if flagOfResult == 1{
-            flagOfResult = 0
-            memoryNumber.text = ""
-            resultLabel.text = "0"
-            numberShowed = resultLabel.text!
-            visnumber = ","
-            calcnumber = "."
-            numberShowed = numberShowed + visnumber
-            numberCalculate = numberCalculate + calcnumber
-            resultLabel.text = numberShowed
-            decimalPressed = 1
+        if errFlag == 1{
+            resultLabel.text = "Err"
         }else{
-            if decimalPressed == 0{
-                decimalPressed = 1
-                if resultLabel.text == "0"{
-                    numberShowed = "0"
-                }else{
-                    numberShowed = resultLabel.text!
-                }
-                visnumber = ","
-                calcnumber = "."
+            if flagOfResult == 1{
+                flagOfResult = 0
+                resultLabel.text! = ""
+                numberShowed = ""
+                memoryNumber.text = ""
+            }
+            number = sender.tag
+            if resultLabel.text == "0"{
+                numberShowed = ""
+                numberCalculate = ""
+            }
+            visnumber = String(number)
+            calcnumber = String(number)
+            if resetPressed == false{
                 numberShowed = numberShowed + visnumber
                 numberCalculate = numberCalculate + calcnumber
                 resultLabel.text = numberShowed
             }
         }
+        
+    }
+    
+    @IBAction func decimalPressed(_ sender: Any) {
+        if errFlag == 1{
+            resultLabel.text = "Err"
+        }else{
+            if flagOfResult == 1{
+                flagOfResult = 0
+                memoryNumber.text = ""
+                resultLabel.text = "0"
+                numberShowed = resultLabel.text!
+                visnumber = ","
+                calcnumber = "."
+                numberShowed = numberShowed + visnumber
+                numberCalculate = numberCalculate + calcnumber
+                resultLabel.text = numberShowed
+                decimalPressed = 1
+            }else{
+                if decimalPressed == 0{
+                    decimalPressed = 1
+                    if resultLabel.text == "0"{
+                        numberShowed = "0"
+                    }else{
+                        numberShowed = resultLabel.text!
+                    }
+                    visnumber = ","
+                    calcnumber = "."
+                    numberShowed = numberShowed + visnumber
+                    numberCalculate = numberCalculate + calcnumber
+                    resultLabel.text = numberShowed
+                }
+            }
+        }
+        
     }
     
     @IBAction func calculation(_ sender: UIButton) {
-        flagOfResult = 0
-        flagArray = [sumFlag,minusFlag,multiplyFlag,divideFlag]
-        if sumFlag != 1 && minusFlag != 1 && multiplyFlag != 1 && divideFlag != 1{
-            switch sender.tag {
-                case 0:
-                    plusButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
-                    decimalPressed = 0
-                    calculatelabel = 0
-                    sumFlag = 1
-                    minusFlag = 0
-                    multiplyFlag = 0
-                    divideFlag = 0
-                case 1:
-                    minusButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
-                    calculatelabel = 1
-                    decimalPressed = 0
-                    sumFlag = 0
-                    minusFlag = 1
-                    multiplyFlag = 0
-                    divideFlag = 0
-                case 2:
-                    multiplyButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
-                    calculatelabel = 2
-                    decimalPressed = 0
-                    sumFlag = 0
-                    minusFlag = 0
-                    multiplyFlag = 1
-                    divideFlag = 0
-                case 3:
-                    divideButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
-                    calculatelabel = 3
-                    decimalPressed = 0
-                    sumFlag = 0
-                    minusFlag = 0
-                    multiplyFlag = 0
-                    divideFlag = 1
-                default:
-                    print("Otherwise, do something else.")
-            }
-            if resultLabel.text == "0" {
-                numberShowed = "0"
-            }
-            numberShowed = resultLabel.text!
-            memoryNumber.text = numberShowed + " \(calculationArray[sender.tag])"
-            if let indic = numberShowed.firstIndex(of: ","){
-                numberShowed.remove(at: indic)
-                numberShowed.insert(".", at: indic)
-            }
-            memoryNumberCalculate = numberShowed
-            resultLabel.text = ""
-            numberShowed = ""
-            numberCalculate = ""
+        if errFlag == 1{
+            resultLabel.text = "Err"
         }else{
-            let indexOfOne = flagArray.firstIndex(of: 1)
-            if sender.tag == indexOfOne{
-                resetButton()
-            }else{
+            flagOfResult = 0
+            flagArray = [sumFlag,minusFlag,multiplyFlag,divideFlag]
+            if sumFlag != 1 && minusFlag != 1 && multiplyFlag != 1 && divideFlag != 1{
                 switch sender.tag {
                     case 0:
-                        resetButton()
                         plusButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
+                        decimalPressed = 0
                         calculatelabel = 0
                         sumFlag = 1
                         minusFlag = 0
                         multiplyFlag = 0
                         divideFlag = 0
                     case 1:
-                        resetButton()
                         minusButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
                         calculatelabel = 1
+                        decimalPressed = 0
                         sumFlag = 0
                         minusFlag = 1
                         multiplyFlag = 0
                         divideFlag = 0
                     case 2:
-                        resetButton()
                         multiplyButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
                         calculatelabel = 2
+                        decimalPressed = 0
                         sumFlag = 0
                         minusFlag = 0
                         multiplyFlag = 1
                         divideFlag = 0
                     case 3:
-                        resetButton()
                         divideButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
                         calculatelabel = 3
+                        decimalPressed = 0
                         sumFlag = 0
                         minusFlag = 0
                         multiplyFlag = 0
@@ -270,31 +230,95 @@ class ViewController: UIViewController {
                     numberShowed.insert(".", at: indic)
                 }
                 memoryNumberCalculate = numberShowed
-                memoryNumberCalculate = numberShowed
-                resultLabel.text = "0"
+                resultLabel.text = ""
                 numberShowed = ""
                 numberCalculate = ""
+            }else{
+                let indexOfOne = flagArray.firstIndex(of: 1)
+                if sender.tag == indexOfOne{
+                    resetButton()
+                }else{
+                    switch sender.tag {
+                        case 0:
+                            resetButton()
+                            plusButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
+                            calculatelabel = 0
+                            sumFlag = 1
+                            minusFlag = 0
+                            multiplyFlag = 0
+                            divideFlag = 0
+                        case 1:
+                            resetButton()
+                            minusButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
+                            calculatelabel = 1
+                            sumFlag = 0
+                            minusFlag = 1
+                            multiplyFlag = 0
+                            divideFlag = 0
+                        case 2:
+                            resetButton()
+                            multiplyButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
+                            calculatelabel = 2
+                            sumFlag = 0
+                            minusFlag = 0
+                            multiplyFlag = 1
+                            divideFlag = 0
+                        case 3:
+                            resetButton()
+                            divideButton.backgroundColor = UIColor.init(red: 1, green: 0.83, blue: 0.61, alpha: 0.4)
+                            calculatelabel = 3
+                            sumFlag = 0
+                            minusFlag = 0
+                            multiplyFlag = 0
+                            divideFlag = 1
+                        default:
+                            print("Otherwise, do something else.")
+                    }
+                    if resultLabel.text == "0" {
+                        numberShowed = "0"
+                    }
+                    numberShowed = resultLabel.text!
+                    memoryNumber.text = numberShowed + " \(calculationArray[sender.tag])"
+                    if let indic = numberShowed.firstIndex(of: ","){
+                        numberShowed.remove(at: indic)
+                        numberShowed.insert(".", at: indic)
+                    }
+                    memoryNumberCalculate = numberShowed
+                    memoryNumberCalculate = numberShowed
+                    resultLabel.text = "0"
+                    numberShowed = ""
+                    numberCalculate = ""
+                }
             }
         }
+        
     }
     
     @IBAction func makeMinus(_ sender: Any) {
-        let minus = "-"
-        if flagOfResult == 1{
-            numberShowed = resultLabel.text!
-        }
-        if (resultLabel.text != "0") && (flagOfMinus == 0){
-            flagOfMinus = 1
-            numberShowed = minus + numberShowed
-            numberCalculate = minus + numberCalculate
-            resultLabel.text = numberShowed
-        }else if (resultLabel.text != "0") && (flagOfMinus == 1){
-            flagOfMinus = 0
-            let range = numberShowed.index(after: numberShowed.startIndex)..<numberShowed.endIndex
-            numberShowed = String(numberShowed[range])
-            let range_ = numberCalculate.index(after: numberCalculate.startIndex)..<numberCalculate.endIndex
-            numberCalculate = String(numberCalculate[range_])
-            resultLabel.text = numberShowed
+        if errFlag == 1{
+            resultLabel.text = "Err"
+        }else{
+            if (resetPressed == false) && (resultLabel.text == "Err"){
+                resultLabel.text = "Err"
+            }else{
+                let minus = "-"
+                if flagOfResult == 1{
+                    numberShowed = resultLabel.text!
+                }
+                if (resultLabel.text != "0") && (flagOfMinus == 0){
+                    flagOfMinus = 1
+                    numberShowed = minus + numberShowed
+                    numberCalculate = minus + numberCalculate
+                    resultLabel.text = numberShowed
+                }else if (resultLabel.text != "0") && (flagOfMinus == 1){
+                    flagOfMinus = 0
+                    let range = numberShowed.index(after: numberShowed.startIndex)..<numberShowed.endIndex
+                    numberShowed = String(numberShowed[range])
+                    let range_ = numberCalculate.index(after: numberCalculate.startIndex)..<numberCalculate.endIndex
+                    numberCalculate = String(numberCalculate[range_])
+                    resultLabel.text = numberShowed
+                }
+            }
         }
     }
     
@@ -319,6 +343,7 @@ class ViewController: UIViewController {
     
     @IBAction func reset(_ sender: Any) {
         resetButton()
+        errFlag = 0
         flagOfResult = 0
         flagOfMinus = 0
         decimalPressed = 0
@@ -353,7 +378,7 @@ class ViewController: UIViewController {
             calculationResult = Double(firstNumber)! * Double(secondNumber)!
         case 3:
             if secondNumber == "0"{
-                memoryNumber.text = ""
+                errFlag = 1
                 resultLabel.text = "Err"
                 return "Err"
             }else{
